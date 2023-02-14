c032073776eb345abfeae062549e6f1bc69efef5
GuillaumeC96 <122291329+GuillaumeC96@users.noreply.github.com>
2023-02-14 16:26:48 +0100

    Add files via upload

---
+++ SQL/EXO_SQL.sql
@@ -0,0 +1,73 @@
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           10.6.11-MariaDB-0ubuntu0.22.04.1 - Ubuntu 22.04
+-- SE du serveur:                debian-linux-gnu
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Listage de la structure de la base pour papyrus
+CREATE DATABASE IF NOT EXISTS `papyrus` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `papyrus`;
+
+-- Listage de la structure de table papyrus. vente
+CREATE TABLE IF NOT EXISTS `vente` (
+  `codart` char(4) NOT NULL,
+  `numfou` int(11) NOT NULL,
+  `delliv` smallint(6) NOT NULL,
+  `qte1` int(11) NOT NULL,
+  `prix1` decimal(5,0) NOT NULL,
+  `qte2` int(11) DEFAULT NULL,
+  `prix2` decimal(5,0) DEFAULT NULL,
+  `qte3` int(11) DEFAULT NULL,
+  `prix3` decimal(5,0) DEFAULT NULL,
+  PRIMARY KEY (`codart`,`numfou`),
+  KEY `numfou` (`numfou`),
+  CONSTRAINT `vente_ibfk_1` FOREIGN KEY (`numfou`) REFERENCES `fournis` (`numfou`),
+  CONSTRAINT `vente_ibfk_2` FOREIGN KEY (`codart`) REFERENCES `produit` (`codart`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Listage des données de la table papyrus.vente : ~27 rows (environ)
+INSERT INTO `vente` (`codart`, `numfou`, `delliv`, `qte1`, `prix1`, `qte2`, `prix2`, `qte3`, `prix3`) VALUES
+	('B001', 8700, 15, 0, 150, 50, 145, 100, 140),
+	('B002', 8700, 15, 0, 210, 50, 200, 100, 185),
+	('D035', 120, 0, 0, 40, 0, 0, 0, 0),
+	('D035', 9120, 5, 0, 40, 100, 30, 0, 0),
+	('I100', 120, 90, 0, 700, 50, 600, 120, 500),
+	('I100', 540, 70, 0, 710, 60, 630, 100, 600),
+	('I100', 9120, 60, 0, 800, 70, 600, 90, 500),
+	('I100', 9150, 90, 0, 650, 90, 600, 200, 590),
+	('I100', 9180, 30, 0, 720, 50, 670, 100, 490),
+	('I105', 120, 90, 10, 705, 50, 630, 120, 500),
+	('I105', 540, 70, 0, 810, 60, 645, 100, 600),
+	('I105', 8700, 30, 0, 720, 50, 670, 100, 510),
+	('I105', 9120, 60, 0, 920, 70, 800, 90, 700),
+	('I105', 9150, 90, 0, 685, 90, 600, 200, 590),
+	('I108', 120, 90, 5, 795, 30, 720, 100, 680),
+	('I108', 9120, 60, 0, 920, 70, 820, 100, 780),
+	('I110', 9120, 60, 0, 950, 70, 850, 90, 790),
+	('I110', 9180, 90, 0, 900, 70, 870, 90, 835),
+	('P220', 120, 15, 0, 3700, 100, 3500, 0, 0),
+	('P220', 8700, 20, 50, 3500, 100, 3350, 0, 0),
+	('P230', 120, 30, 0, 5200, 100, 5000, 0, 0),
+	('P230', 8700, 60, 0, 5000, 50, 4900, 0, 0),
+	('P240', 120, 15, 0, 2200, 100, 2000, 0, 0),
+	('P250', 120, 30, 0, 1500, 100, 1400, 500, 1200),
+	('P250', 9120, 30, 0, 1500, 100, 1400, 500, 1200),
+	('R080', 9120, 10, 0, 120, 100, 100, 0, 0),
+	('R132', 9120, 5, 0, 275, 0, 0, 0, 0);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
